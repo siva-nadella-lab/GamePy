a61cfd082dc1d6a03b58314dccb552e695756695
Sivarama Nadella <155717269+siva-nadella-lab@users.noreply.github.com>
2024-07-20 13:18:10 +0530

    Added Bullet and Alien. Functionality in progress

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import sys
 import pygame
 from settings import Settings as GameSettings
 from ship import Ship
+from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     ## Alien Invasion Class ##
@@ -15,14 +17,38 @@ class AlienInvasion:
         self.clock = pygame.time.Clock()
         if self.gameSettings.full_screen:
             self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
-            self.screen.screen_width = self.screen.get_rect().width
-            self.screen.screen_height = self.screen.get_rect().height
+            self.gameSettings.screen = (self.screen.get_rect().width, self.screen.get_rect().height)
         else:
             self.screen = pygame.display.set_mode(self.gameSettings.screen)
         pygame.display.set_caption(self.gameSettings.caption)
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+        self._createAlienFleet()
 
+    def _createAlienFleet(self):
+        alien = Alien(self)
+        self.aliens.add(alien)
+        alien_width = alien.rect.width
+        alien_height = alien.rect.height
+
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.gameSettings.screen[1] - 3 * alien_height):
+
+            while (current_x < self.gameSettings.screen[0] - 2 * alien_width):
+                self._createAlien(current_x, current_y)
+                current_x += 2*alien_width
+            
+            current_x = alien_width
+            current_y += 2 * alien_height
+
+    def _createAlien(self, xPosition, yPosition):
+        newAlien = Alien(self)
+        newAlien.x = xPosition
+        newAlien.rect.x = xPosition
+        newAlien.rect.y = yPosition
+        self.aliens.add(newAlien)
 
     def run_game(self):
         ## Main loop of the game ##
@@ -30,8 +56,18 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
+
+            self._clear_used_bullets()
+
             self._update_screen()
             self.clock.tick(60)
+
+    def _clear_used_bullets(self):
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <=0:
+                self.bullets.remove(bullet)
+        print ("Bullet Count: " + len(self.bullets).__str__())
 
     def _check_events(self):
         ## Respond to key presses and mouse events ##
@@ -54,8 +90,11 @@ class AlienInvasion:
             self.ship.moving_down =False
 
     def _check_keydown_events(self, event):
+        print(event.key)
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
         elif event.key == pygame.K_UP:
@@ -65,9 +104,19 @@ class AlienInvasion:
         elif event.key == pygame.K_q:
                 sys.exit()
 
+    def _fire_bullet(self):
+        if (len(self.bullets) <= self.gameSettings.max_bullets):
+            newBullet = Bullet(self)
+            self.bullets.add(newBullet)
+
     def _update_screen(self):
         self.screen.fill(self.gameSettings.bgcolor)
         self.ship.blitme()
+
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+
+        self.aliens.draw(self.screen)
 
         pygame.display.flip()
 
--- settings.py
+++ settings.py
@@ -2,9 +2,18 @@ class Settings:
 
     def __init__(self):
         ## Initializing the settings of the game... ##
+
+        ## screen settings ##
         self.bgcolor = (230,230,230)
         self.screen = (1200,800)
         self.full_screen = True
         self.caption = "Alien Invasion"
         self.frame_rate = 60
         self.ship_speed = 4
+
+        ## Bullet Settings ##
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
+        self.max_bullets = 20
